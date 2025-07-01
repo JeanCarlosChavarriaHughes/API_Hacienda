46f0e121966326f052c79ca62eaaf14e4e695284
Jean Chavarria <jeancarlos.chavarria19@gmail.com>
2025-06-30 18:20:55 -0600

    Updating exakat, phpstan, psalm

--- api/modules/wirez/messagesSend.php
+++ api/modules/wirez/messagesSend.php
@@ -28,34 +28,37 @@ function wirez_messagesSend()
     # Get the sender and recipient details
     $sender = wirez_loadContact(params_get('from', ''));
 
-    if ($sender == ERROR_DB_NO_RESULTS_FOUND)
+    if ($sender == ERROR_DB_NO_RESULTS_FOUND) {
         return ERROR_WIREZ_NO_VALID_CONTACT;
+    }
 
     $recipient = wirez_loadContact(params_get('to', ''));
 
-    if ($recipient == ERROR_DB_NO_RESULTS_FOUND)
+    if ($recipient == ERROR_DB_NO_RESULTS_FOUND) {
         return ERROR_WIREZ_NO_VALID_CONTACT;
+    }
 
     # Get the recipient
     # @todo Add wirez correctly
     $recipient = users_load(array('userName' => params_get('to', 0)));
 
     # If there is no conversation, I will create one, but only if there is a subject
-    if (params_get('idConversation', 0) == 0 && params_get('subject', '') != "")
+    if (params_get('idConversation', 0) == 0 && params_get('subject', '') != "") {
         $idConversation = wirez_conversationsCreate($user->idUser, params_get('subject', 0), $recipient->idUser);
-    else
+    } else {
         $idConversation = params_get('idConversation', 0);
+    }
 
     # @ todo CHECK THAT THE CONVERSATION EXISTS AND VALIDATE USER!!!
     $q = sprintf("INSERT INTO wirez_msgs (timestamp, ip, idSender, idRecipient, text, attachments, idConversation)
         VALUES('%s','%s','%s','%s','%s','%s','%s')",
-            time(),
-            db_escape($_SERVER['REMOTE_ADDR']),
-            db_escape($user->idUser),
-            db_escape($recipient->idUser),
-            db_escape(addslashes(json_decode(params_get('text',''), ENT_QUOTES))),
-            '',
-            db_escape($idConversation)
+        time(),
+        db_escape($_SERVER['REMOTE_ADDR']),
+        db_escape($user->idUser),
+        db_escape($recipient->idUser),
+        db_escape(addslashes(json_decode(params_get('text', ''), ENT_QUOTES))),
+        '',
+        db_escape($idConversation)
     );
 
     $r = db_query($q, 0);
@@ -75,7 +78,7 @@ function wirez_conversationsCreate($idUser, $subject, $idRecipient)
         db_escape($idUser),
         db_escape($idRecipient),
         $timestamp,
-        db_escape($subject);
+        db_escape($subject));
 
     $r = db_query($insert, 0);
 
